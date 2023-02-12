c14910bdd23b17b72085a81f43f364764e5f7c6c
bartosz <bartoszbizon1998@gmail.com>
2023-02-12 17:21:32 +0100

    Create new project

---
+++ src/main/java/com/example/Service/panel/API/ServicePanelApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Service.panel.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicePanelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicePanelApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Service/panel/API/ServicePanelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Service.panel.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicePanelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
