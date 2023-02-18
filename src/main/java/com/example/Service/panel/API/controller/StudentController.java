f36d49a1c3a2419f4abd435fa0d77831bfe6e908
bartosz <bartoszbizon1998@gmail.com>
2023-02-18 20:20:04 +0100

    Added and connection database

---
+++ src/main/java/com/example/Service/panel/API/controller/StudentController.java
@@ -0,0 +1,26 @@
+package com.example.Service.panel.API.controller;
+
+import com.example.Service.panel.API.model.Student;
+import com.example.Service.panel.API.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return studentService.getStudents();
+    }
+}
---
+++ src/main/java/com/example/Service/panel/API/service/StudentService.java
@@ -0,0 +1,24 @@
+package com.example.Service.panel.API.service;
+
+import com.example.Service.panel.API.model.Student;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+@Service
+public class StudentService {
+    @GetMapping
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Mariam",
+                        "mariam.jamal@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21
+                )
+        );
+    }
+}
