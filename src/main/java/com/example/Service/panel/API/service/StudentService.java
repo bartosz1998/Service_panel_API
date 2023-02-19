3a16d71f0662172e95d7958651fef77013983971
bartosz <bartoszbizon1998@gmail.com>
2023-02-19 12:39:47 +0100

    trying add function DELETE

--- src/main/java/com/example/Service/panel/API/service/StudentService.java
+++ src/main/java/com/example/Service/panel/API/service/StudentService.java
@@ -4,10 +4,7 @@ import com.example.Service.panel.API.model.Student;
 import com.example.Service.panel.API.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,5 +29,14 @@ public class StudentService {
             throw new IllegalArgumentException("email token");
         }
         studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if(!exists){
+            throw new IllegalStateException(
+                    "student with id " + studentId + " does not exists");
+        }
+        studentRepository.deleteById(studentId);
     }
 }
