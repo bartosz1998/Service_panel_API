585a16ff85f105f0be2f9ce1aa405474090876aa
bartosz <bartoszbizon1998@gmail.com>
2023-02-19 11:52:37 +0100

    date of birth - calculation function

--- src/main/java/com/example/Service/panel/API/model/Student.java
+++ src/main/java/com/example/Service/panel/API/model/Student.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.time.Period;
+
 @Entity
 @Table
 @NoArgsConstructor
@@ -23,6 +25,7 @@ public class Student {
     private String name;
     private String email;
     private LocalDate dob;
+    @Transient
     private Integer age;
 
     public Student(Long id) {
@@ -31,24 +34,20 @@ public class Student {
 
     public Student(String name,
                    String email,
-                   LocalDate dob,
-                   Integer age) {
+                   LocalDate dob) {
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public Student(Long id,
                    String name,
                    String email,
-                   LocalDate dob,
-                   Integer age) {
+                   LocalDate dob) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public Long getId() {
@@ -84,7 +83,7 @@ public class Student {
     }
 
     public Integer getAge() {
-        return age;
+        return Period.between(this.dob, LocalDate.now()).getYears();
     }
 
     public void setAge(Integer age) {
