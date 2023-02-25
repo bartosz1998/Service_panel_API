2144bc573cdfaff5eac26429584bf1c429452253
bartosz <bartoszbizon1998@gmail.com>
2023-02-25 21:17:00 +0100

    add logger text console in debug

--- src/main/java/com/example/Service/panel/API/security/auth/AuthenticationController.java
+++ src/main/java/com/example/Service/panel/API/security/auth/AuthenticationController.java
@@ -1,6 +1,8 @@
 package com.example.Service.panel.API.security.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,18 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
+    private final Logger log = LoggerFactory.getLogger(AuthenticationController.class);
     private final AuthenticationService service;
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request
     ) {
+        log.debug( "REST request to get register user");
         return ResponseEntity.ok(service.register(request));
     }
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(
             @RequestBody AuthenticationRequest request
     ) {
+        log.debug("REST request to post authorize token for user");
         return ResponseEntity.ok(service.authenticate(request));
     }
 }
